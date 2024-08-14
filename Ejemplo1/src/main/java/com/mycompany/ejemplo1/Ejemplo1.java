569ddc0f59ec7716cfe735d43c97a2d305c8950f
mmazariegos-2021338 <mmazariegos-2021338@kinal.edu.gt>
2024-08-13 18:25:01 -0600

    Se agrega el proyecto

---
+++ Ejemplo1/src/main/java/com/mycompany/ejemplo1/Ejemplo1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplo1;
+
+/**
+ *
+ * @author mares
+ */
+public class Ejemplo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
